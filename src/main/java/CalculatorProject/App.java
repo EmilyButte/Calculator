999d5abf743b46049ffe88060ff1ce357ea4dfba
Emily Butte <emily.butte@gmail.com>
2016-09-13 14:36:19 -0400

    added credit to Kyle

--- src/main/java/CalculatorProject/App.java
+++ src/main/java/CalculatorProject/App.java
@@ -3,14 +3,22 @@ package CalculatorProject;
 /**
  * Created by emilybutte on 9/13/16.
  */
+
+// ALL CREDIT DUE TO KYLE JACKSON WHO WALKED ME THROUGH THIS!!!
+// WHEN HE STARTED WORKING WITH ME, I ONLY HAD A COUPLE BASIC MATH METHODS
+// BUT NOW I UNDERSTAND WHAT EVERYTHING DOES AND WHY IT'S NEEDED AND HOW TO MAKE IT
+// HE ALSO TAUGHT ME ABOUT CATCH BLOCKS
+
 public class App {
     public static void main(String[] args) {
+
         Display display = new Display();
 
         try {
             display.CalcRun();
         } catch (Exception e) {
             System.out.println("Enter valid command");
+
             display.CalcRun();
         }
     }
